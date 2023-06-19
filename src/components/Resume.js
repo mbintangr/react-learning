4ac2df741130c1376d4862ea0b4d2e5114f800dc
Bintang Ramadhan <mbintangr13@gmail.com>
2023-06-19 21:23:11 +0700

    Create Resume Component

---
+++ src/components/Resume.js
@@ -0,0 +1,10 @@
+import { Component } from 'react'
+
+class Resume extends Component {
+    render() {
+        const { name } = this.props;
+        return <h2>This is {name}'s Resume</h2>
+    }
+}
+
+export default Resume;
