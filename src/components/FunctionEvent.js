ff6eb04548cfcdf46a681d81decd23fa18dfbad0
Bintang Ramadhan <mbintangr13@gmail.com>
2023-06-19 21:20:17 +0700

    Learn Function Event Handling

---
+++ src/components/FunctionEvent.js
@@ -0,0 +1,15 @@
+const FunctionEvent = () => {
+
+    const handleClick = () => {
+        console.log("Clicked!");    
+    }
+
+    return (
+        <div>
+            Functional Component 
+            <button onClick={handleClick}>Click here!</button>
+        </div>
+    )
+}
+
+export default FunctionEvent;
